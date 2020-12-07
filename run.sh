e687ff58cf8eacacebe1bd3b11e31cafff053e0a
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-12-07 10:08:09 +0300

    Improve run.sh to be able to compile with Scala 2.13's jars

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 if [[ "$1" == "clean" ]]; then
     rm -rf classes
+    rm -rf bacteria.jar
+elif [[ "$1" == "jar" ]]; then
+    java -Dscala.usejavacp=true -cp 'lib/*' scala.tools.nsc.Main -deprecation -sourcepath src -d bacteria.jar src/alife/*
+elif [[ "$1" == "run" ]]; then
+    java -Xmx1G -cp lib/scala-library-2.13.2.jar:bacteria.jar alife.Main
 else
     mkdir -p classes
     scalac -sourcepath src -d classes src/alife/*
