b58415a7f02dc45aeab531bfabf964a481ce708e
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-12-07 05:34:07 +0300

    Move most of configurable stuff to properties.

--- src/alife/Main.scala
+++ src/alife/Main.scala
@@ -2,9 +2,11 @@ package alife
 
 import java.awt.{BorderLayout, Graphics}
 import java.awt.image.BufferedImage
+import java.io.FileReader
 import java.util.concurrent.ThreadLocalRandom
+import java.util.Properties
+
 import javax.swing._
-import alife.Field.Constants
 
 import scala.annotation.tailrec
 
@@ -12,39 +14,53 @@ import scala.annotation.tailrec
  * A first attempt to run the bacteria system
  */
 object Main {
+  private def visualConversion(a: Double): Double = math.log1p(a * (math.E - 1))
+
+  private def loadProperties(args: Array[String]): Properties = {
+    val properties = new Properties()
+    val fileName = if (args.length == 0) "config.properties" else args(0)
+    val propReader = new FileReader(fileName)
+    properties.load(propReader)
+    propReader.close()
+    properties
+  }
+
   def main(args: Array[String]) {
-    val constants = Constants(
-      rotationCost = 0.01,      // what portion of current effective weight is spent on rotation
-      moveCost = 0.02,          // what portion of current effective weight is spend on move
-      eatCost = 0.05,           // what part of food is not actually eaten
-      forkCost = 0.1,           // what part of chromosome weight, as energy, is spend on forking
-      debrisDegradation = 0.02, // what part of debris disappear
-      debrisToEnergy = 0.0,     // what part of debris becomes accessible as energy again
-      synthesisInit = 0.3,      // the expected amount of new food to be generated on first generations
-      synthesisFinal = 0.03,    // the expected amount of new food to be generated on infinitely remote generations
-      synthesisDecay = 0.001,   // how fast the amount of new food moves from first to infinitely remove generations
-      idleCost = 0.1,           // the amount of energy to spend on just being alive
-      healthMultiple = 10,      // how much energy can a bacteria keep, relative to genome size
-      spotPeriodX = 2,          // how many hot spots (in terms of food) in a row will eventually appear
-      spotPeriodY = 1,          // how many how spots in a column will eventually appear
-      spotSpeedX = 0.000094,    // how many horizontal space per generation every hot spot will move
-      spotSpeedY = 0.0002,      // how many vertical space per generation every hot spot will move
-      spotDecay = 0.01          // how fast everything outside the spot will decay
+    val properties = loadProperties(args)
+
+    val constants = Field.Constants(
+      rotationCost = properties.getProperty("rotationCost").toDouble,
+      moveCost = properties.getProperty("moveCost").toDouble,
+      eatCost = properties.getProperty("eatCost").toDouble,
+      forkCost = properties.getProperty("forkCost").toDouble,
+      debrisDegradation = properties.getProperty("debrisDegradation").toDouble,
+      debrisToEnergy = properties.getProperty("debrisToEnergy").toDouble,
+      synthesisInit = properties.getProperty("synthesisInit").toDouble,
+      synthesisFinal = properties.getProperty("synthesisFinal").toDouble,
+      synthesisDecay = properties.getProperty("synthesisDecay").toDouble,
+      idleCost = properties.getProperty("idleCost").toDouble,
+      healthMultiple = properties.getProperty("healthMultiple").toDouble,
+      spotPeriodX = properties.getProperty("spotPeriodX").toDouble,
+      spotPeriodY = properties.getProperty("spotPeriodY").toDouble,
+      spotSpeedX = properties.getProperty("spotSpeedX").toDouble,
+      spotSpeedY = properties.getProperty("spotSpeedY").toDouble,
+      spotDecay = properties.getProperty("spotDecay").toDouble
     )
 
-    val width = 957
-    val height = 520
-    val viewScale = 2
-    val initHealth = 10
-    val genomeLength = 50
+    val width = properties.getProperty("fieldWidth").toInt
+    val height = properties.getProperty("fieldHeight").toInt
+    val viewScale = properties.getProperty("pixelScale").toInt
+    val initialHealth = properties.getProperty("initialHealth").toDouble
+    val initialGenomeLength = properties.getProperty("initialGenomeLength").toInt
+    val initialBacteriaProbability = properties.getProperty("initialBacteriaProbability").toDouble
     val field = new Field(width, height)
 
     var y = 0
     while (y < height) {
       var x = 0
       while (x < width) {
-        if (ThreadLocalRandom.current().nextInt(5) == 0) {
-          field.setGenome(x, y, IndexedSeq.tabulate(genomeLength)(Instruction.random), ThreadLocalRandom.current().nextInt(4), initHealth)
+        if (ThreadLocalRandom.current().nextDouble( ) < initialBacteriaProbability) {
+          field.setGenome(x, y, IndexedSeq.tabulate(initialGenomeLength)(Instruction.random), ThreadLocalRandom.current().nextInt(4), initialHealth)
         }
         x += 1
       }
@@ -52,14 +68,14 @@ object Main {
     }
 
     object View extends JPanel {
-      final val pixelWidth = width * viewScale
-      final val pixelHeight = height * viewScale
-      final val pixels = Array.ofDim[Int](pixelWidth * pixelHeight)
+      final val widthInPixels = width * viewScale
+      final val heightInPixels = height * viewScale
+      final val pixels = Array.ofDim[Int](widthInPixels * heightInPixels)
 
-      private[this] val image = new BufferedImage(pixelWidth, pixelHeight, BufferedImage.TYPE_INT_ARGB)
+      private[this] val image = new BufferedImage(widthInPixels, heightInPixels, BufferedImage.TYPE_INT_ARGB)
 
       def refill(): Unit = {
-        image.setRGB(0, 0, pixelWidth, pixelHeight, pixels, 0, pixelWidth)
+        image.setRGB(0, 0, widthInPixels, heightInPixels, pixels, 0, widthInPixels)
       }
 
       override def paintComponent(g: Graphics): Unit = {
@@ -90,15 +106,15 @@ object Main {
       while (y < height) {
         var x = 0
         while (x < width) {
-          val h = (field.getHealth(x, y) * 255 / maxHealth).toInt
-          val e = (field.getEnergy(x, y) * 255 / maxEnergy).toInt
-          val d = (field.getDebris(x, y) * 255 / maxEnergy).toInt
+          val h = (visualConversion(field.getHealth(x, y) / maxHealth) * 255).toInt
+          val e = (visualConversion(field.getEnergy(x, y) / maxEnergy) * 255).toInt
+          val d = (visualConversion(field.getDebris(x, y) / maxEnergy) * 255).toInt
           val z = (h << 16) | (e << 8) | d | 0xff000000
           var dx = 0
           while (dx < viewScale) {
             var dy = 0
             while (dy < viewScale) {
-              View.pixels(viewScale * x + dx + (viewScale * y + dy) * View.pixelWidth) = z
+              View.pixels(viewScale * x + dx + (viewScale * y + dy) * View.widthInPixels) = z
               dy += 1
             }
             dx += 1
