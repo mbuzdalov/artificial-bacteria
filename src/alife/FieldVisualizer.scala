ab57ba8c684d45558569dec6967c73b988c4d5a7
Maxim Buzdalov <mbuzdalov@gmail.com>
2020-12-07 05:52:57 +0300

    Extract field visualizer from the main class.

---
+++ src/alife/FieldVisualizer.scala
@@ -0,0 +1,69 @@
+package alife
+
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+import javax.swing.JPanel
+
+class FieldVisualizer(field: Field, pixelScale: Int) extends JPanel {
+  private[this] final val widthInPixels = field.width * pixelScale
+  private[this] final val heightInPixels = field.height * pixelScale
+  private[this] final val pixels = Array.ofDim[Int](widthInPixels * heightInPixels)
+
+  private[this] var maxHealth = 0.0
+  private[this] var maxEnergy = 0.0
+  private[this] val image = new BufferedImage(widthInPixels, heightInPixels, BufferedImage.TYPE_INT_ARGB)
+
+  private def visualConversion(a: Double): Double = math.log1p(a * (math.E - 1))
+
+  def fetchField(): Unit = {
+    maxHealth *= 0.95
+    maxEnergy *= 0.95
+
+    val height = field.height
+    val width = field.width
+
+    var y = 0
+    while (y < height) {
+      var x = 0
+      while (x < width) {
+        maxHealth = math.max(maxHealth, field.getHealth(x, y))
+        maxEnergy = math.max(maxEnergy, field.getEnergy(x, y))
+        maxEnergy = math.max(maxEnergy, field.getDebris(x, y))
+        x += 1
+      }
+      y += 1
+    }
+    y = 0
+    while (y < height) {
+      var x = 0
+      while (x < width) {
+        val h = (visualConversion(field.getHealth(x, y) / maxHealth) * 255).toInt
+        val e = (visualConversion(field.getEnergy(x, y) / maxEnergy) * 255).toInt
+        val d = (visualConversion(field.getDebris(x, y) / maxEnergy) * 255).toInt
+        val z = (h << 16) | (e << 8) | d | 0xff000000
+        var dx = 0
+        while (dx < pixelScale) {
+          var dy = 0
+          while (dy < pixelScale) {
+            pixels(pixelScale * x + dx + (pixelScale * y + dy) * widthInPixels) = z
+            dy += 1
+          }
+          dx += 1
+        }
+        x += 1
+      }
+      y += 1
+    }
+    image.synchronized {
+      image.setRGB(0, 0, widthInPixels, heightInPixels, pixels, 0, widthInPixels)
+    }
+    repaint()
+  }
+
+  override def paintComponent(g: Graphics): Unit = {
+    super.paintComponent(g)
+    image.synchronized {
+      g.drawImage(image, 0, 0, this)
+    }
+  }
+}
--- src/alife/Main.scala
+++ src/alife/Main.scala
@@ -1,7 +1,6 @@
 package alife
 
-import java.awt.{BorderLayout, Frame, Graphics}
-import java.awt.image.BufferedImage
+import java.awt.{BorderLayout, Frame}
 import java.io.FileReader
 import java.util.concurrent.ThreadLocalRandom
 import java.util.Properties
@@ -12,8 +11,6 @@ import scala.annotation.tailrec
  * A first attempt to run the bacteria system
  */
 object Main {
-  private def visualConversion(a: Double): Double = math.log1p(a * (math.E - 1))
-
   private def loadProperties(args: Array[String]): Properties = {
     val properties = new Properties()
     val fileName = if (args.length == 0) "config.properties" else args(0)
@@ -47,7 +44,7 @@ object Main {
 
     val width = properties.getProperty("fieldWidth").toInt
     val height = properties.getProperty("fieldHeight").toInt
-    val viewScale = properties.getProperty("pixelScale").toInt
+    val pixelScale = properties.getProperty("pixelScale").toInt
     val initialHealth = properties.getProperty("initialHealth").toDouble
     val initialGenomeLength = properties.getProperty("initialGenomeLength").toInt
     val initialBacteriaProbability = properties.getProperty("initialBacteriaProbability").toDouble
@@ -65,77 +62,19 @@ object Main {
       y += 1
     }
 
-    object View extends JPanel {
-      final val widthInPixels = width * viewScale
-      final val heightInPixels = height * viewScale
-      final val pixels = Array.ofDim[Int](widthInPixels * heightInPixels)
-
-      private[this] val image = new BufferedImage(widthInPixels, heightInPixels, BufferedImage.TYPE_INT_ARGB)
-
-      def refill(): Unit = {
-        image.setRGB(0, 0, widthInPixels, heightInPixels, pixels, 0, widthInPixels)
-      }
-
-      override def paintComponent(g: Graphics): Unit = {
-        super.paintComponent(g)
-        g.drawImage(image, 0, 0, View)
-      }
-    }
-
-    var maxHealth = 100.0
-    var maxEnergy = 100.0
-
-    def fillImage(): Unit = {
-      maxHealth *= 0.95
-      maxEnergy *= 0.95
-
-      var y = 0
-      while (y < height) {
-        var x = 0
-        while (x < width) {
-          maxHealth = math.max(maxHealth, field.getHealth(x, y))
-          maxEnergy = math.max(maxEnergy, field.getEnergy(x, y))
-          maxEnergy = math.max(maxEnergy, field.getDebris(x, y))
-          x += 1
-        }
-        y += 1
-      }
-      y = 0
-      while (y < height) {
-        var x = 0
-        while (x < width) {
-          val h = (visualConversion(field.getHealth(x, y) / maxHealth) * 255).toInt
-          val e = (visualConversion(field.getEnergy(x, y) / maxEnergy) * 255).toInt
-          val d = (visualConversion(field.getDebris(x, y) / maxEnergy) * 255).toInt
-          val z = (h << 16) | (e << 8) | d | 0xff000000
-          var dx = 0
-          while (dx < viewScale) {
-            var dy = 0
-            while (dy < viewScale) {
-              View.pixels(viewScale * x + dx + (viewScale * y + dy) * View.widthInPixels) = z
-              dy += 1
-            }
-            dx += 1
-          }
-          x += 1
-        }
-        y += 1
-      }
-      View.refill()
-      View.repaint()
-    }
+    val view = new FieldVisualizer(field, pixelScale)
 
     val window = new JFrame("Bacteria")
     window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
     window.setLayout(new BorderLayout())
-    window.add(View, BorderLayout.CENTER)
+    window.add(view, BorderLayout.CENTER)
     window.setExtendedState(Frame.MAXIMIZED_BOTH)
     window.setUndecorated(true)
     window.setVisible(true)
 
     @tailrec
     def work(generation: Int): Unit = if (window.isVisible) {
-      fillImage()
+      view.fetchField()
       print(f"$generation: #bacteria: ${field.getNumberOfBacteria}%5d, max size: ${field.getMaxGenomeSize}%2d")
       if (field.getNumberOfBacteria > 0) {
         val actionsMade = field.simulationStep(constants, generation)
