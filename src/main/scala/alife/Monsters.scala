ee95fff06c671831e7afbd604844993511da1439
Maxim Buzdalov <mbuzdalov@gmail.com>
2026-09-19 03:20:47 +0100

    Add a couple of new monsters.

--- src/main/scala/alife/Monsters.scala
+++ src/main/scala/alife/Monsters.scala
@@ -10,6 +10,10 @@ object Monsters:
     IndexedSeq(Fork(Smooth), RotateMinus, RotatePlus, Move, Eat) -> IndexedSeq(
       // of minimal length, works very quickly but is sometimes unreliable
       IArray(FoodAt(0), MyHealth, HealthAt(1), Log(3), Times(4,3)),
+      // this did not give a chance to the previous monster
+      IArray(MyHealth, Sin(1), FoodAt(0), DebrisAt(0), Sigmoid(0,1), HealthAt(8), Minus(3,5)),
+      // this was an evolution of the previous one
+      IArray(FoodAt(0), Plus(1,1), MyHealth, FoodAt(3), Sin(2), Times(3,5)),
     ),
     IndexedSeq(Eat, Move, Fork(Smooth), RotatePlus, RotateMinus) -> IndexedSeq(
       IArray(FoodAt(0), MyWeight, HealthAt(0), Const(0.2594246799612059), HealthAt(2), Log(3), Exp(3), Times(6,7)),
